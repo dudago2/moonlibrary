b937ac59a2a83f654624d39c61bec5c0bd1d2612
A11223027 <a11223027@yuntech.edu.tw>
2024-06-11 14:27:29 +0800

    meminfodone

--- library 4/borrow/meminfo.js
+++ library 4/borrow/meminfo.js
@@ -1,3 +1,19 @@
  function back() {
     window.location.href = 'borrow.html';
  }
+
+ function record() {
+   var record = document.getElementById("record");
+   var collect = document.getElementById("collect");
+
+   record.style.display = "block";  
+   collect.style.display = "none";
+}
+
+function collect() {
+   var record = document.getElementById("record");
+   var collect = document.getElementById("collect");
+
+   record.style.display = "none";  
+   collect.style.display = "block";
+}
