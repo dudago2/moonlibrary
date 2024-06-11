6c3782a0c065c0be6d021a050ab8cc0bcf0a8520
dudago2 <a11223010@gemail.yuntech.edu.tw>
2024-06-11 14:52:30 +0800

    書本查詢連接後端

--- library 4/borrow/borrow.js
+++ library 4/borrow/borrow.js
@@ -55,26 +55,40 @@ function home() {
 // 查詢
 const books = [
     { title: "To Kill a Mockingbird", author: "Harper Lee", subject: "英語", image: "https://i.postimg.cc/yNVb8Mpj/10267-bk11713946761.jpg"},
-    { title: "1984", author: "George Orwell", subject: "漫畫"},
-    { title: "The Great Gatsby", author: "F. Scott Fitzgerald", subject: "小說"},
-    { title: "One Hundred Years of Solitude", author: "Gabriel Garcia Marquez", subject: "旅遊"},
-    { title: "Moby Dick", author: "Herman Melville", subject: "歷史"}
+    { title: "1984", author: "George Orwell", subject: "漫畫", image: "https://i.postimg.cc/yNVb8Mpj/10267-bk11713946761.jpg"},
+    { title: "The Great Gatsby", author: "F. Scott Fitzgerald", subject: "小說", image: "https://i.postimg.cc/yNVb8Mpj/10267-bk11713946761.jpg"},
+    { title: "One Hundred Years of Solitude", author: "Gabriel Garcia Marquez", subject: "旅遊", image: "https://i.postimg.cc/yNVb8Mpj/10267-bk11713946761.jpg"},
+    { title: "Moby Dick", author: "Herman Melville", subject: "歷史", image: "https://i.postimg.cc/yNVb8Mpj/10267-bk11713946761.jpg"}
 ];
 
 function bookSearch() {
     const query = document.getElementById('searchInput1').value.toLowerCase();
-        const resultsContainer = document.getElementById('results1');
-        resultsContainer.innerHTML = '';
-
+    console.log(query);
+    //var getBookDetail;
+    const resultsContainer = document.getElementById('results1');
+    resultsContainer.innerHTML = '';
+    fetch('http://localhost:8080/api/books/search?title='+query, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
         if (query) {
             document.getElementById('error_message1').style.display = 'none';
-            const filteredBooks = books.filter(book => book.title.toLowerCase().includes(query));
-
-            if (filteredBooks.length > 0) {
-                filteredBooks.forEach(book => {
+            //const filteredBooks = books.filter(book => book.title.toLowerCase().includes(query));
+            if (data.length > 0) {
+                data.forEach(book => {
                     const bookElement = document.createElement('div');
                     bookElement.className = 'book';
-                    bookElement.innerHTML = `<h1>書名：${book.title}</h3><p>作者：${book.author}</p><p>主題：${book.subject}</p><img src="${book.image}"/>`;
+                    bookElement.innerHTML = `<h1>書名：${book.title}</h1>
+                                            <p>作者：${book.author}</p>
+                                            <p>主題：${book.subject}</p>
+                                            <img src="${book.url}"/>`;
                     resultsContainer.appendChild(bookElement);
                 });
             } else {
@@ -83,52 +97,100 @@ function bookSearch() {
         } else {
             document.getElementById('error_message1').style.display = 'block';
         }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('error_message').style.display = 'block';
+    });
+       
 }
 
 function authorSearch() {
-    const query = document.getElementById('searchInput2').value.toLowerCase();
-        const resultsContainer = document.getElementById('results2');
-        resultsContainer.innerHTML = '';
 
+    const query = document.getElementById('searchInput2').value.toLowerCase();
+    console.log(query);
+    //var getBookDetail;
+    const resultsContainer = document.getElementById('results2');
+    resultsContainer.innerHTML = '';
+    fetch('http://localhost:8080/api/books/search/author?author='+query, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
         if (query) {
             document.getElementById('error_message2').style.display = 'none';
-            const filteredBooks = books.filter(book => book.author.toLowerCase().includes(query));
-
-            if (filteredBooks.length > 0) {
-                filteredBooks.forEach(book => {
+            //const filteredBooks = books.filter(book => book.title.toLowerCase().includes(query));
+            if (data.length > 0) {
+                data.forEach(book => {
                     const bookElement = document.createElement('div');
                     bookElement.className = 'book';
-                    bookElement.innerHTML = `<h1>書名：${book.title}</h3><p>作者：${book.author}</p><p>主題：${book.subject}</p><img src="${book.image}"/>`;
+                    bookElement.innerHTML = `<h1>書名：${book.title}</h1>
+                                            <p>作者：${book.author}</p>
+                                            <p>主題：${book.subject}</p>
+                                            <img src="${book.url}"/>`;
                     resultsContainer.appendChild(bookElement);
                 });
             } else {
-                resultsContainer.innerHTML = '<p>無此作者，請重新查詢</p>';
+                resultsContainer.innerHTML = '<p>無此書名，請重新查詢</p>';
             }
         } else {
             document.getElementById('error_message2').style.display = 'block';
         }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('error_message').style.display = 'block';
+    });
 }
 
 function subjectSearch() {
+   
     const query = document.getElementById('searchInput3').value.toLowerCase();
-        const resultsContainer = document.getElementById('results3');
-        resultsContainer.innerHTML = '';
-
+    console.log(query);
+    //var getBookDetail;
+    const resultsContainer = document.getElementById('results3');
+    resultsContainer.innerHTML = '';
+    fetch('http://localhost:8080/api/books/search/subject?subject='+query, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
         if (query) {
             document.getElementById('error_message3').style.display = 'none';
-            const filteredBooks = books.filter(book => book.subject.toLowerCase().includes(query));
-
-            if (filteredBooks.length > 0) {
-                filteredBooks.forEach(book => {
+            //const filteredBooks = books.filter(book => book.title.toLowerCase().includes(query));
+            if (data.length > 0) {
+                data.forEach(book => {
                     const bookElement = document.createElement('div');
                     bookElement.className = 'book';
-                    bookElement.innerHTML = `<h1>書名：${book.title}</h3><p>作者：${book.author}</p><p>主題：${book.subject}</p><img src="${book.image}"/>`;
+                    bookElement.innerHTML = `<h1>書名：${book.title}</h1>
+                                            <p>作者：${book.author}</p>
+                                            <p>主題：${book.subject}</p>
+                                            <img src="${book.url}"/>`;
                     resultsContainer.appendChild(bookElement);
                 });
             } else {
-                document.getElementById('error_message3').style.display = 'block';
+                resultsContainer.innerHTML = '<p>無此書名，請重新查詢</p>';
             }
+        } else {
+            document.getElementById('error_message3').style.display = 'block';
         }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('error_message').style.display = 'block';
+    });
 }
 
  function member() {
